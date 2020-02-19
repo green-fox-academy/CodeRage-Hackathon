382c03ffb759e3fd9126001da4aad64048da04bd
Elaiden <ghorvath84@gmail.com>
2020-02-19 19:33:08 +0100

    initial project added

---
+++ src/main/java/com/hackathon/coderage/toolboxproject/ToolboxprojectApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.coderage.toolboxproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolboxprojectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ToolboxprojectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hackathon/coderage/toolboxproject/ToolboxprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.coderage.toolboxproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolboxprojectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
