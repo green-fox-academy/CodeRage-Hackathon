48398aa00e7484dab73823121443c8c02bf3ef54
Tamás Markovics <53872735+mrkvcstms@users.noreply.github.com>
2020-02-20 17:26:52 +0100

    refactor(ToolServiceImpl): changed service to match controller needs

--- src/main/java/com/hackathon/coderage/toolboxproject/tool/ToolService.java
+++ src/main/java/com/hackathon/coderage/toolboxproject/tool/ToolService.java
@@ -1,12 +1,14 @@
 package com.hackathon.coderage.toolboxproject.tool;
 
+import com.hackathon.coderage.toolboxproject.dto.ToolRequestDTO;
+import com.hackathon.coderage.toolboxproject.exceptions.MissingParameterException;
 import java.util.List;
 
 public interface ToolService {
 
   List<Tool> findAvailable(long startTime, long endTime, String type, int number);
 
-  Tool addTool(String name, int hourlyPrice);
+  Tool addTool(ToolRequestDTO requestDTO) throws MissingParameterException;
 
   void removeToolById(long id);
 
