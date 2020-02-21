ced49e22bf5c749301f0511a3792253621ce069d
Tamás Markovics <53872735+mrkvcstms@users.noreply.github.com>
2020-02-21 03:53:01 +0100

    fix: added errorhandling to removeTool

--- src/main/java/com/hackathon/coderage/toolboxproject/tool/ToolService.java
+++ src/main/java/com/hackathon/coderage/toolboxproject/tool/ToolService.java
@@ -1,6 +1,7 @@
 package com.hackathon.coderage.toolboxproject.tool;
 
 import com.hackathon.coderage.toolboxproject.dto.ToolRequestDTO;
+import com.hackathon.coderage.toolboxproject.exceptions.IncorrectIdException;
 import com.hackathon.coderage.toolboxproject.exceptions.MissingParameterException;
 import java.util.List;
 
@@ -8,7 +9,7 @@ public interface ToolService {
 
   Tool addTool(ToolRequestDTO requestDTO) throws MissingParameterException;
 
-  void removeToolById(long id);
+  void removeToolById(long id) throws IncorrectIdException;
 
   List<Tool> findAllByName(String name);
 
