73392326dfbef6994f62c95fb76384dbb40355d1
Tamás Markovics <53872735+mrkvcstms@users.noreply.github.com>
2020-02-21 08:42:18 +0100

    testtest

--- src/test/java/com/hackathon/coderage/toolboxproject/appuser/RegisterLoginIntegrationTest.java
+++ src/test/java/com/hackathon/coderage/toolboxproject/appuser/RegisterLoginIntegrationTest.java
@@ -21,7 +21,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class RegisterLoginIntegrationTest {
 
