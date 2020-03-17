3b31a13c78e7c5de711b3e8e1381914d7866390d
Tamás Markovics <53872735+mrkvcstms@users.noreply.github.com>
2020-03-18 00:55:20 +0100

    refactor(OrderServiceImpl): update with new JobType, add more sensible logic

--- src/main/java/com/hackathon/coderage/toolboxproject/order/OrderServiceImpl.java
+++ src/main/java/com/hackathon/coderage/toolboxproject/order/OrderServiceImpl.java
@@ -7,12 +7,16 @@ import com.hackathon.coderage.toolboxproject.dto.OrdersResponseDTO;
 import com.hackathon.coderage.toolboxproject.exceptions.IncorrectJobTypeException;
 import com.hackathon.coderage.toolboxproject.exceptions.NoEmployeeAvailableException;
 import com.hackathon.coderage.toolboxproject.exceptions.NoToolAvailableException;
+import com.hackathon.coderage.toolboxproject.jobtype.JobType;
+import com.hackathon.coderage.toolboxproject.jobtype.JobTypeService;
 import com.hackathon.coderage.toolboxproject.tool.Tool;
 import com.hackathon.coderage.toolboxproject.tool.ToolService;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,12 +25,16 @@ public class OrderServiceImpl implements OrderService {
   private OrderRepository orderRepository;
   private AppUserService appUserService;
   private ToolService toolService;
+  private JobTypeService jobTypeService;
 
   public OrderServiceImpl(OrderRepository orderRepository,
-      AppUserService appUserService, ToolService toolService) {
+      AppUserService appUserService,
+      ToolService toolService,
+      JobTypeService jobTypeService) {
     this.orderRepository = orderRepository;
     this.appUserService = appUserService;
     this.toolService = toolService;
+    this.jobTypeService = jobTypeService;
   }
 
   @Override
@@ -50,37 +58,56 @@ public class OrderServiceImpl implements OrderService {
     return new OrdersResponseDTO(appUser.getOrders());
   }
 
-  private Set<AppUser> findAvailableEmployeesOnDate(long date) throws NoEmployeeAvailableException {
+  private List<AppUser> findAvailableEmployeesOnDate(long date) {
     List<AppUser> employees = new ArrayList<>(this.appUserService.findAllEmployees());
     this.orderRepository.findAllByDate(date)
         .forEach(order -> order.getEmployees()
             .forEach(employees::remove));
-    if (employees.size() == 0) {
-      throw new NoEmployeeAvailableException();
-    }
-    return new HashSet<>(employees);
-  }
-
-  private Set<Tool> findAvailableToolsOnDate(long date) throws NoToolAvailableException {
-    List<Tool> tools = new ArrayList<>(this.toolService.findAll());
-    this.orderRepository.findAllByDate(date)
-        .forEach(order -> order.getTools()
-            .forEach(tools::remove));
-    if (tools.size() == 0) {
-      throw new NoToolAvailableException();
-    }
-    return new HashSet<>(tools);
+    return employees;
   }
 
   @Override
   public Order createOrder(OrderRequestDTO request, String customerName)
       throws NoEmployeeAvailableException, NoToolAvailableException, IncorrectJobTypeException {
-    // TODO date validation
+    JobType jobType = this.jobTypeService.findJobType(request.getType());
+    Set<AppUser> employees = this.findCrewForOrder(request.getDate(), jobType);
+    Set<Tool> tools = findToolsForOrder(request.getDate(), jobType);
     AppUser customer = this.appUserService.findByUsername(customerName);
-    Set<Tool> tools = this.findAvailableToolsOnDate(request.getDate());
-    Set<AppUser> employees = this.findAvailableEmployeesOnDate(request.getDate());
-    Order order = OrderFactory.orderJob(request.getType(), employees, tools, request);
-    order.setCustomer(customer);
-    return this.orderRepository.save(order);
+
+    return this.orderRepository.save(new Order(customer, employees, tools, request.getDate()));
+  }
+
+  private Set<AppUser> findCrewForOrder(long date, JobType jobType)
+      throws NoEmployeeAvailableException {
+    Set<AppUser> employees = this.findAvailableEmployeesOnDate(date).stream()
+        .filter(employee -> employee.getQualification()
+            .contains(jobType.getRequiredQualification()))
+        .limit(jobType.getCrewSize())
+        .collect(Collectors.toSet());
+    if (employees.size() != jobType.getCrewSize()) {
+      throw new NoEmployeeAvailableException();
+    }
+    return employees;
+  }
+
+  private Set<Tool> findToolsForOrder(long date, JobType jobType) throws NoToolAvailableException {
+    Set<Tool> availableTools = new HashSet<>(this.toolService.findAll());
+    this.orderRepository.findAllByDate(date)
+        .forEach(order -> order.getTools()
+            .forEach(availableTools::remove));
+
+    Set<Tool> foundTools = new HashSet<>();
+    String[] requiredTools = jobType.getRequiredTools().split(";");
+    for (String tool : requiredTools) {
+      Optional<Tool> tempTool = availableTools.stream()
+          .filter(item -> item.getName().equalsIgnoreCase(tool))
+          .findFirst();
+      if (tempTool.isEmpty()) {
+        throw new NoToolAvailableException();
+      }
+      foundTools.add(tempTool.get());
+      availableTools.remove(tempTool.get());
+    }
+    return foundTools;
   }
 }
