8db2954cabf2cc7fc0049893afc51ed252518eb7
Tamás Markovics <53872735+mrkvcstms@users.noreply.github.com>
2020-03-20 01:06:44 +0100

    feat(BadInputException): add input validation exception

---
+++ src/main/java/com/hackathon/coderage/toolboxproject/exceptions/BadInputException.java
@@ -0,0 +1,8 @@
+package com.hackathon.coderage.toolboxproject.exceptions;
+
+public class BadInputException extends Exception {
+
+  public BadInputException() {
+    super("Invalid input, please try again!");
+  }
+}
