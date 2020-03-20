112f81e118bfeaa943eb7e054f8f5da8c02dcec2
Tamás Markovics <53872735+mrkvcstms@users.noreply.github.com>
2020-03-20 02:14:10 +0100

    feat(JobTypeController): add updateJobType

--- src/main/java/com/hackathon/coderage/toolboxproject/jobtype/JobTypeController.java
+++ src/main/java/com/hackathon/coderage/toolboxproject/jobtype/JobTypeController.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,18 +26,30 @@ public class JobTypeController {
     this.jobTypeService = jobTypeService;
   }
 
-  @GetMapping("/jobs")
+  @GetMapping("/job_types")
   public ResponseEntity<List<JobTypeResponseDTO>> listAllJobTypes() {
     return ResponseEntity.status(HttpStatus.OK)
         .body(this.jobTypeService.listAllJobTypesInDTO());
   }
 
-  @PostMapping("/job")
+  @PostMapping("/job_type")
   public ResponseEntity<ResponseDTO> createJobType(
       @RequestBody JobTypeRequestDTO requestDTO) {
     try {
       return ResponseEntity.status(HttpStatus.OK)
           .body(new JobTypeResponseDTO(this.jobTypeService.createJobType(requestDTO)));
+    } catch (BadInputException e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body(new ErrorResponseDTO("error", e.getMessage()));
+    }
+  }
+
+  @PutMapping("/job_type/{name}")
+  public ResponseEntity<ResponseDTO> updateJobType(@PathVariable(name = "name") String name,
+      @RequestBody(required = false) JobTypeRequestDTO requestDTO) {
+    try {
+      return ResponseEntity.status(HttpStatus.OK)
+          .body(new JobTypeResponseDTO(this.jobTypeService.updateJobType(requestDTO, name)));
     } catch (BadInputException e) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST)
           .body(new ErrorResponseDTO("error", e.getMessage()));
